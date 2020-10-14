c73f717b47bc9fb5079bd14229d0a9e8526400bf
Samuele <44635579+wiztoth@users.noreply.github.com>
2020-10-14 09:20:35 +0200

    Update Shutdown-AllNodeESX-VMs.sh

--- Shutdown-AllNodeESX-VMs.sh
+++ Shutdown-AllNodeESX-VMs.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
-#prima connessione al nodo esx02 e inventario delle virtual machines sul nodo
-ssh root@192.168.55.134 'esxcli vm process list;' > '/home/samuele/Desktop/ssh script/vms.txt'
+# A bash script to shutdown every virtual machines on esx VMware node coded from Samuele Leone
+#first of all, connecting to node and collecting virtual machines on that node esx in VMware infrastructure
+ssh user@ip_of_node 'esxcli vm process list;' > '/home/user/Desktop/ssh script/vms.txt'
 #----------------------------------------------------------------------------
-# rilevo quante VMs sono in totale sul nodo, questo servirà nel ciclo di spengimento per sapere quante volte effettuare il ciclo.
-length=$(cat '/home/samuele/Desktop/ssh script/vms.txt' | grep -o -i 'World ID' | wc -l)
+# counting vms on that node, that number it's useful for shutdown iteration 
+length=$(cat '/home/user/Desktop/ssh script/vms.txt' | grep -o -i 'World ID' | wc -l)
 #----------------------------------------------------------------------------
-# Da riga 9 a 15, si filtra escludendo nomi e dati inutili, così da ottenere tutti i World ID che sono passati nel ciclo di spengimento
-array=$(cat '/home/samuele/Desktop/ssh script/vms.txt' | grep 'World ID' )
+# Here from row 9 to 15 i filter every kind of data that's useless for my scope. I just want World ID to shutdown virtual machines.
+array=$(cat '/home/user/Desktop/ssh script/vms.txt' | grep 'World ID' )
 for each in "${array[@]}"
 do
-  echo "$each" | awk '{print $3}' > '/home/samuele/Desktop/ssh script/vms.txt'
+  echo "$each" | awk '{print $3}' > '/home/user/Desktop/ssh script/vms.txt'
 done
-IDtemp=$(cat '/home/samuele/Desktop/ssh script/vms.txt' )
-#parte il ciclo di spengimento e trasformo gli IDs in un singolo ID per volta per ogni iterazione del ciclo
+IDtemp=$(cat '/home/user/Desktop/ssh script/vms.txt' )
+# Here loop to shutdown every vm from his world id 
 for (( i = 1; i <= $length; i++ )); do
   #statements
    ID=$(echo $IDtemp | cut -d ' ' -f $i)
    ssh root@192.168.55.134 "esxcli vm process kill --type soft --world-id $ID"
 done
-# E' consigliabile applicare un certificato SSH pubblico che non chiede sempre la password perchè per ogni comando ssh dovrà essere inserita manualmente la password
-#fine dello script - È RELATIVO SOLO AL NODO 2 ESX , VA COPIATO ANCHE PER IL NODO 1 CAMBIANDO I RELATIVI DATI DI CONNESSIONE
+# Tips on that script: using a public certificate could make it fastest to run it, otherwise every run of the script will ask you password.
+# That's all , obviously here i consider as my target just one esx node, if you wanna to shutdown vms on other node you have to connect to every node in infrastructure.
